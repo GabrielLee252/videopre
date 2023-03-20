777bb76d2cc18a3016f8161ce9835d65b119446b
Gabe Lee <gibgab6@gmail.com>
2023-03-20 17:15:16 -0400

    muted the video and got it  to play when hovered

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("page loaded...");
+
+const video = document.getElementById('vid');
+
+video.addEventListener('mouseover', ()=>{
+    video.muted = true;
+    video.play()
+});
+
+video.addEventListener('mouseout', ()=>{
+    video.muted = false;
+    video.pause()
+});
