4dc404ca96337e929eef61f5c7706eb8b3e7274c
Gabe Lee <gibgab6@gmail.com>
2023-03-20 18:33:31 -0400

    tried both ways

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ console.log("page loaded...");
 
 const video = document.getElementById('vid');
 
-video.addEventListener('mouseover', ()=>{
-    video.muted = true;
-    video.play()
-});
+// video.addEventListener('mouseover', ()=>{
+//     video.muted = true;
+//     video.play()
+// });
 
-video.addEventListener('mouseout', ()=>{
-    video.muted = false;
-    video.pause()
-});
+// video.addEventListener('mouseout', ()=>{
+//     video.muted = false;
+//     video.pause()
+// });
+
+
+function play(element){
+    element.play();
+}
+
+function pause(element){
+    element.pause(;)
+}
